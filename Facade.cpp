c4374afb1e3ebc494df5f4e05392c24c0fcaaeb6
jejaeddy <jeja12@live.com>
2016-03-01 15:58:58 -0600

    Actualizacion y renombre Facade.cpp

---
+++ Facade.cpp
@@ -0,0 +1,74 @@
+/*
+ * Facade.cpp
+ *
+ *  Created on: 28 de feb. de 2016
+ *      Author: jose
+ */
+
+#include <SFML/Graphics.hpp>
+#include "Facade.h"
+#include "../Interfaz/Interfaz.h"
+
+LinkedList<sf::RectangleShape>* Facade::_Rect = new LinkedList<sf::RectangleShape>();
+
+LinkedList<sf::CircleShape>* Facade::_Circle = new LinkedList<sf::CircleShape>();
+
+Facade::Facade() {
+	this->Rect;
+	this->Circle;
+}
+
+void Facade::start(){
+		_Juego = new NewGame();
+		Interfaz *_Interfaz = new Interfaz(_Rect,_Circle);
+		_Juego->start();
+		_Interfaz->run();
+}
+
+void Facade::crear(){
+	//Creacion de obejetos bloques
+	for(int indexBloque = 0; indexBloque < GameConstants::NUMERO_DE_ELEMENTOS_EN_MATIRIZ; indexBloque++){
+		int columna = indexBloque/GameConstants::NUMERO_DE_COLUMNAS;
+		int fila = indexBloque%GameConstants::NUMERO_DE_COLUMNAS;
+		Bloques bloque = _Juego->getMatrizDeBloques()->getData(columna,fila);
+		if(bloque.get_Visible()){
+			this->Rect.setSize(sf::Vector2f(GameConstants::LARGO_DEL_BLOQUE,GameConstants::ANCHO_DEL_BLOQUE));
+			this->Rect.setPosition(columna*GameConstants::LARGO_DEL_BLOQUE,fila*GameConstants::ANCHO_DEL_BLOQUE);
+			this->Rect.setOutlineThickness(1);
+			this->Rect.setOutlineColor(sf::Color::Black);
+			switch(bloque.get_tipo()){
+			case 0:
+				this->Rect.setFillColor(sf::Color::White);
+				break;
+			case 1:
+				this->Rect.setFillColor(sf::Color::Blue);
+				break;
+			case 2:
+				this->Rect.setFillColor(sf::Color::Yellow);
+				break;
+			case 3:
+				this->Rect.setFillColor(sf::Color::Red);
+				break;
+			case 4:
+				this->Rect.setFillColor(sf::Color::Cyan);
+				break;
+			}
+			this->_Rect->insertAtFinal(this->Rect);
+		}
+	}
+
+	//Creacion de objetos bola
+	//for(int indexbola = 0; index)
+}
+
+void Facade::dibujar(){
+	for(int index = 0; index < this->_Rect->len(); index++){
+		Interfaz::_Ventana.draw(this->_Rect->searchBegin());
+		_Rect->deleteAtBegin();
+	}
+}
+
+Facade::~Facade() {
+	// TODO Auto-generated destructor stub
+}
+
