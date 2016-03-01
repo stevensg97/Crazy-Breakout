b0ce691d44e712f964efacd3df0504a55c1ab6bc
jejaeddy <jeja12@live.com>
2016-03-01 16:00:22 -0600

    Actualizacion GameConstants.h

--- GameConstants.h
+++ GameConstants.h
@@ -13,9 +13,9 @@ using namespace std;
 class GameConstants{
 public:
 	const string NO_HAY_MSJ = "NOHAYMENSAJES";
-	static const int NUMERO_DE_ELEMENTOS_EN_MATIRIZ = 25;
-	static const int NUMERO_DE_COLUMNAS = 5;
-	static const int NUMERO_DE_FILAS = 5;
+	static const int NUMERO_DE_ELEMENTOS_EN_MATIRIZ = 100;
+	static const int NUMERO_DE_COLUMNAS = 10;
+	static const int NUMERO_DE_FILAS = 10;
 	static const int LARGO_DE_LA_PANTALLA = 500;
 	static const int ANCHO_DE_LA_PANTALLA = 600;
 	static const int LARGO_DEL_BLOQUE = 50;
@@ -24,7 +24,3 @@ public:
 
 };
 
-
-
-
-#endif /* GAMECONSTANTS_H_ */
