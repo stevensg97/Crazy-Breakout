b36b405c58290107f600aa92e90a31e8970324c6
jejaeddy <jeja12@live.com>
2016-03-02 10:12:29 -0600

    Documentacion de Facade.h

--- Facade.h
+++ Facade.h
@@ -1,8 +1,9 @@
-/*
- * Facade.h
- *
- *  Created on: 28 de feb. de 2016
- *      Author: jose
+ /**
+ * @file Facade.h
+ * @date 28/2/16
+ * @author Jose Eduardo Jimenez A 2014072577
+ * @title Facade
+ * @brief Facade entre las clase NewGame e Interfaz
  */
 
 #ifndef FACADE_H_
@@ -18,15 +19,31 @@
 
 class Facade {
 public:
+	/**
+	 * @brief Metodo constructor de Facade
+	 */
 	Facade();
+	/**
+	 * @brief Metodo destructor de Facade
+	 */
 	virtual ~Facade();
 
+	/**
+	 * @breif Metodo crea los objetos a dibujar
+	 */
 	void crear();
-	void dibujar();
+	/**
+	 * @brief Metodo inicia el Facade
+	 */
 	void start();
 
-
+	/**
+	 * @brief Argumento estatico de RectangleShape
+	 */
 	static LinkedList<sf::RectangleShape> *_Rect;
+	/**
+	 * @brief Argumento estatico de CircleShape
+	 */
 	static LinkedList<sf::CircleShape> *_Circle;
 
 private:
