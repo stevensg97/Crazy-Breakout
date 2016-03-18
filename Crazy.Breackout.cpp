b1c8838dd7dbd98f6b1270be114f0fb320a9e23e
jejaeddy <jeja12@live.com>
2016-03-18 09:14:46 -0600

    Casos de prueba

--- Crazy.Breackout.cpp
+++ Crazy.Breackout.cpp
@@ -1,13 +1,31 @@
 
 #include <iostream>
-#include "../Facade/Facade.h"
-#include "../Interfaz/Interfaz.h"
+#include "../Interfaz/Menu.h"
+#include "../Structures/Matriz.h"
 
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl;
-	Facade *facade = new Facade();
-	facade->start();
+	Menu *menu = new Menu();
+	menu->run();
+//	Matriz<int>* matriz = new Matriz<int>(5,5);
+//	cout << "inicial"<< endl;
+//	matriz->printMatriz();
+//	matriz->insertData(2,3,1);
+//	matriz->insertData(1,3,2);
+//	matriz->insertData(4,4,3);
+//	matriz->insertData(2,4,4);
+//	matriz->insertData(3,3,5);
+//	matriz->insertData(2,1,6);
+//	matriz->insertData(1,1,7);
+//	matriz->insertData(0,0,8);
+//	cout << "insertar"<< endl;
+//	matriz->printMatriz();
+//	matriz->deleteData(4,4);
+//	matriz->deleteData(2,3);
+//	matriz->deleteData(2,1);
+//	matriz->deleteData(1,3);
+//	cout << "borrar"<< endl;
+//	matriz->printMatriz();
 	return 0;
 }
